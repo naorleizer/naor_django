b1c551722269272c820bc09b2d6ad7c40420db80
Naor Leizer <naor0naor@gmail.com>
2023-03-17 20:01:48 +0200

    added content and styled home page

--- naor_django/naor_django/settings.py
+++ naor_django/naor_django/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Settings for the livereload addon
+LIVERELOAD_HOST = '127.0.0.1'
